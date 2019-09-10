4462868402726924b5929f593780a6481412609c
Emilia Elm <emilia.elm@hotmail.com>
2019-09-10 16:52:51 +0200

    added test for math.ceil

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -32,7 +32,10 @@ public class math extends org.python.types.Module {
     }
 
     @org.python.Method(
-         __doc__ = "TODO",
+         __doc__ = "math.ceil(number) -> number" + 
+                   "\n" +
+                   "Return the cealing of number as float, " + 
+                   "the smallest integer value greater than or equal to number.\n",
          args = {"number"}
     )
     public static org.python.Object ceil(org.python.Object number) {
@@ -41,13 +44,13 @@ public class math extends org.python.types.Module {
         }
         if (number instanceof org.python.types.Float) {
             double val = ((org.python.types.Float) number).value; 
-            return new org.python.types.Float(Math.ceil(val));
+            return org.python.types.Int.getInt((int)Math.ceil(val));
         } 
         if (number instanceof org.python.types.Bool) {
             boolean val = ((org.python.types.Bool) number).value;
             return org.python.types.Int.getInt(val ? 1 : 0);
         }
-        throw new org.python.exceptions.TypeError("Must be real number, not "+ number.typeName());
+        throw new org.python.exceptions.TypeError("must be real number, not "+ number.typeName());
 
     }
     @org.python.Method(
