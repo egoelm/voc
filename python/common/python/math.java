5c6a1ae5f2db12892050d1f68dc7f3331337f8b5
andraste <andra_stefania@yahoo.com>
2019-09-10 17:08:53 +0200

    added floor function

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -1,8 +1,9 @@
-package python;
+package org.python.types;
+
 
 @org.python.Module(
         __doc__ =
-                "TODO"
+                "floor()"
 )
 public class math extends org.python.types.Module {
     public math() {
@@ -16,7 +17,7 @@ public class math extends org.python.types.Module {
     /*@org.python.Attribute
     public static org.python.Object __loader__ = org.python.types.NoneType.NONE;  // TODO
     @org.python.Attribute
-    public static org.python.Object __name__ = new org.python.types.Str("time");
+    public static org.python.Object __name__ = new org.python.types.Str("math");
     @org.python.Attribute
     public static org.python.Object __package__ = new org.python.types.Str("");
     @org.python.Attribute
@@ -24,12 +25,31 @@ public class math extends org.python.types.Module {
     public static org.python.types.Int altzone;*/
 
 
+
     @org.python.Method(
-         __doc__ = "TODO"
+            __doc__ = "floor(number)\n" +
+                    "\n" +
+                    "Returns floor(number).  The argument may be\n" +
+                    ".\n",
+            args = {"num"}
     )
-    public static org.python.Object floor() {
-        return org.python.types.Int.getInt(4);
+
+    public static org.python.Object floor(org.python.Object num) {
+         //  org.python.types.Float(0);
+        if (num instanceof org.python.types.Int)
+            {    return(num);}
+        if (num instanceof org.python.types.Float)
+            { double val =  ((org.python.types.Float) num).value;
+             return  new org.python.types.Float(Math.floor(val));
+             }
+        if (num instanceof org.python.types.Bool)
+           {boolean val = ((org.python.types.Bool)num).value;
+           return org.python.types.Int.getInt(val ? 1 : 0);
+           }
+         throw new org.python.exceptions.TypeError("Must be a number, boolean or float");
+
     }
+
 
     @org.python.Method(
          __doc__ = "TODO"
