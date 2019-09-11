5d562a59f47c5ba4e0f1cfd6ebd428ee6cb27282
zupercool <karolin.gjothlen.8133@student.uu.se>
2019-09-11 11:23:17 +0200

    added tests for fabs()

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -1,8 +1,5 @@
 from unittest import expectedFailure
-
-from ..utils import TranspileTestCase
-
-
+from .utils import TranspileTestCase
 
 class MathModuleTests(TranspileTestCase):
     def test_ceil(self):
@@ -13,13 +10,27 @@ class MathModuleTests(TranspileTestCase):
             print(math.ceil(False))
             print(math.ceil(-1))
             """)
-    
+
+    def test_fabs(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.fabs(-1))
+            print(math.fabs(-2.0))
+            print(math.fabs(0))
+            """)
+            
     @expectedFailure
     def test_ceil_expectedFailure(self):
         self.assertCodeExecution("""
             import math
             print(math.ceil("hej"))
             print(math.ceil([]))
-            """)        
-
-
+            """)
+            
+    @expectedFailure
+    def test_fabs_expectedFailure(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.fabs("hej"))
+            print(math.fabs([]))
+            """)
