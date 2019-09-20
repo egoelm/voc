1fbae7c262d9a3345d1b3324f8c2ddee28eaa3f0
Nnairis <Siriann.g@gmail.com>
2019-09-20 11:15:44 +0200

    nullpointer handling

--- python/common/org/python/stdlib/datetime/TimeDelta.java
+++ python/common/org/python/stdlib/datetime/TimeDelta.java
@@ -1,9 +1,11 @@
 package org.python.stdlib.datetime;
 
+import javax.swing.plaf.basic.BasicSplitPaneUI.KeyboardDownRightHandler;
+
 public class TimeDelta extends org.python.types.Object {
-    private org.python.types.Object days;
-    private org.python.types.Object seconds;
-    private org.python.types.Object microseconds;
+    private org.python.Object days;
+    private org.python.Object seconds;
+    private org.python.Object microseconds;
 
 
     @org.python.Method(
@@ -11,35 +13,47 @@ public class TimeDelta extends org.python.types.Object {
     )
     public TimeDelta(org.python.Object[] args, java.util.Map<java.lang.String, org.python.Object> kwargs)  { //gets kwargs to get by keyword yr,minth
         super();
-        /*if(args.length < 0 && kwargs.size() < 0){
-          this.days = (org.python.types.Object)0 ;
-        }*/
+
+        this.days = kwargs.get("days");
+        if (this.days != null && args.length > 0) {
+            throw new org.python.exceptions.SyntaxError("positional argument follows keyword argument");
+        }
+
+        this.seconds = kwargs.get("seconds");
+        if (this.seconds != null && args.length > 0) {
+            throw new org.python.exceptions.SyntaxError("positional argument follows keyword argument");
+        }
+
+        this.microseconds = kwargs.get("microseconds");
+        if (this.microseconds != null && args.length > 0) {
+            throw new org.python.exceptions.SyntaxError("positional argument follows keyword argument");
+        }
 
         if(!kwargs.isEmpty()){
           // A week is converted to 7 days.
-          long weeks = ((org.python.types.Int)kwargs.get("weeks")).value;
-          this.days = org.python.types.Int.getInt(weeks*7);
+          if(kwargs.get("weeks") != null){
+            long weeks = ((org.python.types.Int)kwargs.get("weeks")).value;
+            this.days = org.python.types.Int.getInt(weeks*7);
+          }
 
           // An hour is converted to 3600 seconds.
+          if(kwargs.get("hours") != null){
           long hours = ((org.python.types.Int)kwargs.get("hours")).value;
           this.seconds = org.python.types.Int.getInt(hours*3600);
+          }
 
           // A minute is converted to 60 seconds.
+          if(kwargs.get("minutes") != null){
           long minutes = ((org.python.types.Int)kwargs.get("minutes")).value;
           this.seconds = org.python.types.Int.getInt(minutes*60);
+          }
 
           // A millisecond is converted to 1000 microseconds.
+          if(kwargs.get("milliseconds") != null){
           long millisecond = ((org.python.types.Int)kwargs.get("millisecond")).value;
           this.microseconds = org.python.types.Int.getInt(millisecond*1000);
+          }
         }
-
-      /*  for (org.python.Object elem: args){
-        System.out.println(elem);
-        System.out.println(kwargs.get("year"));
-        System.out.println(kwargs.get("month"));*/
-
-        //this.year = new java.util.  //new java.util.LinkedHashMap<org.python.Object, org.python.Object>();
-
     }
 
     @org.python.Method(
