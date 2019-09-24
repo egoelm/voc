4ad9d0180db1216b3a62b942b0f29d1c3b9c5caf
andraste <andra_stefania@yahoo.com>
2019-09-24 09:50:24 +0200

    clean tests

--- tests/stdlib/test_datetime.py
+++ tests/stdlib/test_datetime.py
@@ -6,65 +6,9 @@ from ..utils import TranspileTestCase
 
 # class TimeDeltaTests(TranspileTestCase):
 
-class DateTimeTests(TranspileTestCase):
-
-    def test_creation(self):
-       self.assertCodeExecution("""
-           from datetime import datetime
-           print(datetime(4, 10, day=11))
-           print(datetime(4, 10, 11))
-           print(datetime(4, month=10, day=11))
-           print(datetime(year=4, month=10, day=11))
-           print(datetime(14, 10, day=11))
-           print(datetime(14, 10, 11))
-           print(datetime(14, month=10, day=11))
-           print(datetime(year=14, month=10, day=11))
-           print(datetime(141, 10, day=11))
-           print(datetime(141, 10, 11))
-           print(datetime(141, month=10, day=11))
-           print(datetime(year=141, month=10, day=11))
-           print(datetime(1413, 10, day=11))
-           print(datetime(1413, 10, 11))
-           print(datetime(1413, month=10, day=11))
-           print(datetime(year=1413, month=10, day=11))
-           """)
-
-    # The following test will not succeed because the microseconds will differ.
-    # def test_today(self):
-    #     self.assertCodeExecution("""
-    #         from datetime import datetime
-    #         print(datetime.today())
-    #     """)
-    
-    def test_date(self):
-        self.assertCodeExecution("""
-            from datetime import datetime
-            print(datetime(1993, 5, 17).date())
-            print(datetime(13, 2, 20).date())
-            print(datetime(1700, 6, 3).date())
-            print(datetime(400, 1, 28).date())
-            print(datetime(2007, 3, 17).date())
-            print(datetime(1843, 12, 1).date())
-            print(datetime(4000, 11, 10).date())
-        """)
-
-    def test_class_attributes(self):
-        self.assertCodeExecution("""
-            from datetime import datetime
-            print(datetime.min)
-            print(datetime.max)
-        """)
+#class DateTimeTests(TranspileTestCase):
 
 
-    # TODO: fix exceptions
-    # def test_creation_invalid(self):
-    #     self.assertCodeExecution("""
-    #         from datetime import datetime
-    #         try:
-    #             datetime(14, month=10, 11)
-    #         except SyntaxError as e:
-    #             print(e)
-    #         """)
 
 class DateTests(TranspileTestCase):
 
