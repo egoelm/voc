ed540f3666e07b59b421b8cce49a3c6d1302d4a5
egoelm <emilia.elm@hotmail.com>
2019-10-09 07:25:36 +0200

    created workload for function __add__ in java

--- java_tests/org/python/java_tests/workloads/Add.java
+++ java_tests/org/python/java_tests/workloads/Add.java
@@ -2,17 +2,26 @@ package org.python.java_tests.workloads;
 
 public class Add {
 	public static void main(String[] args){
-
-		org.python.types.List list =  new org.python.types.List();
+		long totalTime = 0;
+		int runs = 5;
+		long startTime; 
+		org.python.types.List list = null;
+		
+		for(int i = 0; i<runs; i++) {
+			startTime = System.currentTimeMillis();
+			list =  new org.python.types.List();
+			for(int k = 0; k < 10000; k++ ) {
+				org.python.types.List temp = new org.python.types.List();
+				temp.append(org.python.types.Int.getInt(k));
+				list = (org.python.types.List) list.__add__(temp);
+			}
+			totalTime += System.currentTimeMillis() - startTime;
+			
+		}
+		
     	
-    	
-    	list.append(org.python.types.Int.getInt(1));
-    	list.append(org.python.types.Int.getInt(2));
-    	list.append(org.python.types.Int.getInt(3));
-    	list.append(org.python.types.Int.getInt(4));
-    	list.append(org.python.types.Int.getInt(5));
-    	
-    	System.out.println(list);
+    	System.out.println(list.__len__());
+    	System.out.println("Time in seconds " + (totalTime/runs)/1000);
 		
 	}
 }
