b7d7a37f9501df811f69973d8d20a00b34dd87d1
andraste <andra_stefania@yahoo.com>
2019-10-09 13:51:05 +0200

    updates remove workload in Java

--- java_tests/org/python/java_tests/workloads/Remove.java
+++ java_tests/org/python/java_tests/workloads/Remove.java
@@ -4,37 +4,24 @@ public class Remove {
 	public static void main(String[] args) {
 
 		org.python.types.List list =  new org.python.types.List();
-		int len = 49000;
+		int len = 20000;
 		int i;
 		for(i= 0; i<len+20; i++) {
-			list.append(org.python.types.Int.getInt(i));
-			
+			list.append(org.python.types.Int.getInt(i));	
 		}
-		int runs = 100;
-		org.python.types.Bool bool_false = org.python.types.Bool.getBool(false);
+			
 		long startTime = System.currentTimeMillis();
-		org.python.types.Object lenobj;
-		lenobj=org.python.types.Int.getInt(len);
-		for(int x = 0; x<runs;x++) {
-		for(int j= 0; j<len; j++) {
-			//System.out.println("j="+ j);
+
+		for(int j= 0; j<len; j++) {	
 			org.python.types.Object val;
-			val=org.python.types.Int.getInt(len-j);
-			//System.out.println(val);
-			if (lenobj.__lt__(val) != bool_false){
-				list.remove(val);}
-			
+			val=org.python.types.Int.getInt(len-j);	
+			list.remove(val);	
 		}
-		}
-	
-
-		long endTime = System.currentTimeMillis();
-
-		long timeElapsed = (endTime - startTime)/runs;
-
 		
-		System.out.println("Time in seconds " + (timeElapsed/runs)/1000);
-		System.out.println(timeElapsed);
+		long endTime = System.currentTimeMillis();
+		long timeElapsed = (endTime - startTime);
+		System.out.println("Time in seconds " + (timeElapsed/1000));
+		
 		
 	}
 }
