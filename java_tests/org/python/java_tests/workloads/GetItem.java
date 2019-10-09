0585269896c41ad065f7c215c5ca158baf842c98
zupercool <karolin.gjothlen.8133@student.uu.se>
2019-10-09 19:46:40 +0200

    less calls for getint()

--- java_tests/org/python/java_tests/workloads/GetItem.java
+++ java_tests/org/python/java_tests/workloads/GetItem.java
@@ -4,17 +4,24 @@ public class GetItem {
 	public static void main(String[] args) {
 
 		org.python.types.List list = new org.python.types.List();
-		int len = 200000;
+		int len = 10000;
+
+		java.util.ArrayList<org.python.types.Int> indexes = new java.util.ArrayList();
+		
 		for(int i=0; i<len+20; i++) {
-			list.append(org.python.types.Int.getInt(i));	
+			org.python.types.Int j = org.python.types.Int.getInt(i);
+			list.append(j);
+			indexes.add(j);
 		}
-			
+		
 		long startTime = System.currentTimeMillis();
 
-		for(int j=0; j<len; j++) {	
-			org.python.types.Object i;
-			i = org.python.types.Int.getInt(j);
-			list.__getitem__(i);
+		for(int k=0; k<10000; k++) {
+			for(int j=0; j<len; j++) {	
+				org.python.types.Object i;
+				i = indexes.get(j);
+				list.__getitem__(i);
+			}
 		}
 		
 		long endTime = System.currentTimeMillis();
