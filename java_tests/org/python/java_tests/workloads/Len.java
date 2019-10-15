e279ddf39ee3a89a1dc5a8dc95a601421608688f
egoelm <emilia.elm@hotmail.com>
2019-10-15 14:57:54 +0200

    added varible type to len

--- java_tests/org/python/java_tests/workloads/Len.java
+++ java_tests/org/python/java_tests/workloads/Len.java
@@ -2,16 +2,16 @@ package org.python.java_tests.workloads;
 
 public class Len {
 	public static void main(String[] args) {
-		len = 14000;
-		totalTime = 0;
-		runs = 8;
+		int len = 14000;
+		int totalTime = 0;
+		int runs = 8;
 		org.python.types.List list = new org.python.types.List();
 		
-		for(i= 0; i<len; i++) {
+		for(int i= 0; i<len; i++) {
 			list.append(org.python.types.Int.getInt(i));	
 		}
 		
-		for(i = 0; i<runs; i++) {
+		for(int i = 0; i<runs; i++) {
 			long startTime = System.currentTimeMillis();
 			list.__len__();
 			totalTime += System.currentTimeMillis() - startTime;
