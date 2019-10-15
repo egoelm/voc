4528898ac6e719557c9464baca42f6a11dd96356
Nnairis <Siriann.g@gmail.com>
2019-10-15 13:13:47 +0200

    javatest for len

---
+++ java_tests/org/python/java_tests/workloads/Len.java
@@ -0,0 +1,22 @@
+package org.python.java_tests.workloads;
+
+public class Len {
+	public static void main(String[] args) {
+		len = 14000;
+		totalTime = 0;
+		runs = 8;
+		org.python.types.List list = new org.python.types.List();
+		
+		for(i= 0; i<len; i++) {
+			list.append(org.python.types.Int.getInt(i));	
+		}
+		
+		for(i = 0; i<runs; i++) {
+			long startTime = System.currentTimeMillis();
+			list.__len__();
+			totalTime += System.currentTimeMillis() - startTime;
+		}
+
+    	System.out.println("Time in seconds " + (totalTime/runs)/1000);
+	}
+}
