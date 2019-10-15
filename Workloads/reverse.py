55c6d7349a6e9b8c5f270ee3ea46d6c5dfe8e699
Andreas Ivarsson <Ivarsson.Andreas@hotmail.se>
2019-10-15 13:53:12 +0200

    add reverse workload

--- Workloads/reverse.py
+++ Workloads/reverse.py
@@ -3,14 +3,14 @@ from time import time
 
 
 def workload(list):
-    list.reverse()
+    for i in 100:
+        list.reverse()
 
 
 totalTime = 0
 runs = 6
 for i in range(runs):
-    for len in range(1, 1000):
-        list = [i for i in range(len)]
+        list = [i for i in range(1000000)]
         start = time()
         workload(list)
         totalTime += time() - start
---
+++ java_tests/org/python/java_tests/workloads/Reverse.java
@@ -0,0 +1,34 @@
+package org.python.java_tests.workloads;
+
+public class Reverse {
+	
+	private static org.python.Object workload(org.python.types.List list) {
+		for(int i = 0; i < 1000 ; i++) {
+			list.reverse();
+		}
+		return list;
+	}
+	
+	private static org.python.types.List setup(int length) {
+		org.python.types.List list = new org.python.types.List();
+		for (int i = 1; i <= length; i++) {
+				 list.append(org.python.types.Int.getInt(i));
+		}
+		
+
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		org.python.types.List lists = Reverse.setup(100000);
+		
+		long startTime = System.nanoTime();
+		Reverse.workload(lists);
+		long endTime = System.nanoTime();
+		//System.out.println(lists);
+		System.out.println((endTime - startTime) / 1000000); //milliseconds 
+		
+	
+	}
+	
+}
